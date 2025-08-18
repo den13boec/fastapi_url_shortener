9deb9fe7ac7fd209296566850703c6f312530300
den13boec <den-boris@list.ru>
2025-08-18 18:42:05 +0300

    Add db config

---
+++ app/core/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+class Settings:
+    # path to data folder with db using path for config.py
+    DB_DIR = Path(__file__).resolve().parent.parent.parent / "data"
+    # create folder if not already
+    DB_DIR.mkdir(parents=True, exist_ok=True)
+    # join paths
+    DATABASE_URL = f"sqlite:///{DB_DIR / 'db.sqlite3'}"
+
+settings = Settings()
