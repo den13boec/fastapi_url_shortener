885b463ce86861cd40d08a9681c7f8703c74b4a5
den13boec <den-boris@list.ru>
2025-08-18 18:45:58 +0300

    Add database connection

---
+++ app/database.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Session
+from app.core.config import settings
+
+engine = create_engine(
+    settings.DATABASE_URL,
+    # remove connection restriction
+    connect_args={"check_same_thread": False},
+    pool_pre_ping=True,
+)
+
+class Base(DeclarativeBase):
+    pass
+
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False, expire_on_commit=False)
+
+def get_db() -> Session:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
