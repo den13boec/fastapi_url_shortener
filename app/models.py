f7fa04350ea36355f08df6bc61318ddd823e2a2b
den13boec <den-boris@list.ru>
2025-08-22 17:18:36 +0300

    Timezone fix

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlalchemy import Integer, String, Text, DateTime, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 from app.database import Base
@@ -14,4 +14,4 @@ class Link(Base):
     code: Mapped[str] = mapped_column(String(6), nullable=False, index=True, unique=True)
     original_url: Mapped[str] = mapped_column(Text, nullable=False)
     clicks: Mapped[int] = mapped_column(Integer, nullable=False, default=0)
-    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.utcnow)
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, default=lambda: datetime.now(timezone.utc))
